999f4ca42d6edcabc061cad5796291dc88795f9a
Li Zhang <fff008@gmail.com>
2014-04-04 17:35:40 -0400

    10 dot foraging

--- Analysis3/Temp/ForagingCond.m
+++ Analysis3/Temp/ForagingCond.m
@@ -1,25 +1,35 @@
-function [ fp,dots,dotrfidx,dotfig,rfigidx ] = ForagingCond( rfpos,israndom,repn,p,cn,rootpath )
+function [ fp,dots,dotrfidx,dotfigtype,rfigidx ] = ForagingCond( rfpos,isdprand,dprepn,...
+    dotfigtypep,rewardfigtype,rewardfigtypep,rewardfigtypen,condn,filepath )
 %FORAGINGCOND Summary of this function goes here
 %   Detailed explanation goes here
 
-FigType = {'Distractor','Target'};
-
-for cidx=1:cn
-    [ dots,dotrfidx ] = RFPattern1_1( rfpos,israndom,repn );
+rewardfigtypen = 1;
+% [ dots,dotrfidx ] = RFPattern1_1( rfpos,isdprand,dprepn );
+[ dots,dotrfidx ] = RFPattern1_2( rfpos,isdprand );
+dn = length(dots);
+israndrewardfigtype = length(rewardfigtype)>1;
+randrewardfigtypeidx = Analysis.Base.drv(rewardfigtypep,condn,true);
+for cidx=1:condn
     [ fp,dots ] = randtransDP( dots,0.5,0.1 );
-    %plotDP(dots);
-    dn = length(dotrfidx);
-    dotfig = Analysis.Base.drv(p,dn);
-    rfigidx = rewardfig(dotfig,2,1);
+    %     plotDP(dots);
+    %     dotfigtype = Analysis.Base.drv(dotfigtypep,dn);
+    dotfigtype = Analysis.Base.drv(dotfigtypep,dn,true);
+    if israndrewardfigtype
+        crewardfigtype = rewardfigtype(randrewardfigtypeidx(cidx));
+    else
+        crewardfigtype = rewardfigtype;
+    end
+    rfigidx = rewardfig(dotfigtype,crewardfigtype,rewardfigtypen);
     for i=1:dn
         s{i,1} = ['POSX_',num2str(i),' ',num2str(dots(i,1))];
         s{i,2} = ['POSY_',num2str(i),' ',num2str(dots(i,2))];
         s{i,3} = ['PRFIDX_',num2str(i),' ',num2str(dotrfidx(i))];
-        s{i,4} = ['PFIGTYPE_',num2str(i),' ',num2str(dotfig(i)-1)];
+        s{i,4} = ['PFIGTYPE_',num2str(i),' ',num2str(dotfigtype(i)-1)];
     end
     
     ss = reshape(s,1,[]);
     ss = [{['FPOSH',' ',num2str(fp(1)),', ','FPOSV',' ',num2str(fp(2))]} ss];
+    ss{1,end+1} = ['REWARDTYPE',' ',num2str(crewardfigtype-1)];
     ss{1,end+1} = ['REWARDIDX',' ',num2str(find(rfigidx == 1))];
     c{cidx,1} = ss{1};
     for i=2:length(ss)
@@ -28,15 +38,18 @@ for cidx=1:cn
     
 end
 
-isr='';
-if israndom
-    isr = '_randpos';
+rf = ['_rf=(',num2str(rfpos(1)),',',num2str(rfpos(2)),')'];
+if isdprand
+    rf = [rf,'dpr'];
 end
-tp = ['_tp',num2str(p(2))];
+ftp = ['_ftp=(',regexprep(num2str(dotfigtypep),'\s*',','),')'];
+rft = ['_rft=(',regexprep(num2str(rewardfigtype),'\s*',','),')'];
+rftp = ['_rftp=(',regexprep(num2str(rewardfigtypep),'\s*',','),')'];
 
-cfile = ['Foraging',isr,'_',num2str(dn),tp,'_',num2str(cn),'.vlc'];
-fid = fopen(fullfile(rootpath,cfile),'w');
-for cidx=1:cn
+
+cfile = ['Foraging_',num2str(dn),rf,ftp,rft,rftp,'_',num2str(condn),'.vlc'];
+fid = fopen(fullfile(filepath,cfile),'w');
+for cidx=1:condn
     fprintf(fid,'%s\r\n',c{cidx,1});
 end
 fclose(fid);
--- Analysis3/Temp/rewardfig.m
+++ Analysis3/Temp/rewardfig.m
@@ -1,4 +1,4 @@
-function [ rfigidx ] = rewardfig( df,figtype,rn )
+function [ rfigidx ] = rewardfig( dotfigtype,rfigtype,rn )
 %REWARDFIG Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -6,10 +6,13 @@ if nargin < 3
     rn = 1;
 end
 
-fidx = find(df==figtype);
-rs = Analysis.Base.randseq(1,length(fidx),'shuffle');
-rfigidx = zeros(size(df));
-rfigidx(fidx(rs(1:rn)))=1;
+rftidx = find(dotfigtype==rfigtype);
+if isempty(rftidx)
+    error('No Reward Figure Type in Figure Array.');
+end
+rs = Analysis.Base.randseq(1,length(rftidx),'shuffle');
+rfigidx = zeros(size(dotfigtype));
+rfigidx(rftidx(rs(1:rn)))=1;
 
 end
 
