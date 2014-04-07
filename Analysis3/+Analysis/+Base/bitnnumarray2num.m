4d62a9765942cc0a1661986792058c462510a1e3
Li Zhang <fff008@gmail.com>
2014-04-07 19:50:15 -0400

    bit n encoded int array <-> int
    pack/unpack bit n encoded int number array into/from a big int number

---
+++ Analysis3/+Analysis/+Base/bitnnumarray2num.m
@@ -0,0 +1,32 @@
+function [ num,numarray ] = bitnnumarray2num( n,array,endianness )
+%BITNNUMARRAY2NUM Summary of this function goes here
+%   Detailed explanation goes here
+
+if nargin < 3
+    endianness = 'big';
+end
+bitnrange =[0 2^n-1];
+if max(array)>bitnrange(2) || min(array) < bitnrange(1)
+    error(['array exceed the range of bit ',num2str(n),' encoding: ',num2str(bitnrange(1)),' - ',num2str(bitnrange(2))]);
+end
+
+numarray = array;
+m = length(array);
+for i=1:m
+    switch endianness
+        case 'little'
+            t = i-1;
+            idx = m-t;
+        case 'big'
+            t = m-i;
+            idx = i;
+        otherwise
+            error('need correct endianness: little/big');
+    end
+    numarray(idx) = 2^(t*n)*array(i);
+end
+
+num = sum(numarray);
+
+disp([endianness,'-endian encoding done in ',num2str(m*n),' bits']);
+end
---
+++ Analysis3/+Analysis/+Base/num2bitnnumarray.m
@@ -0,0 +1,29 @@
+function [ array ] = num2bitnnumarray( n,num,m,endianness )
+%NUM2BITNNUMARRAY Summary of this function goes here
+%   Detailed explanation goes here
+
+if nargin < 3
+    endianness = 'big';
+end
+maxbits = 64;
+
+bits = bitget(num,maxbits:-1:1);
+bits = bits(end-m*n+1:end);
+bits = reshape(bits,n,m)';
+
+array = zeros(1,m);
+for i = 1:m
+    switch endianness
+        case 'little'
+            idx = m-(i-1);
+        case 'big'
+            idx = i;
+        otherwise
+            error('need correct endianness: little/big');
+    end
+    bstring = regexprep(num2str(bits(idx,:)),'\s*','');
+    array(i) = bin2dec(bstring);
+end
+
+end
+
